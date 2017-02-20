4a6cd1d3bcbe8a62f65844797e31c0e899d8ec96
John <giovannibitar@gmail.com>
2017-02-20 16:51:54 +0100

    created bookings model and migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220145438) do
+ActiveRecord::Schema.define(version: 20170220154850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.boolean  "pending"
+    t.integer  "user_id"
+    t.integer  "car_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string   "model"
@@ -41,5 +53,7 @@ ActiveRecord::Schema.define(version: 20170220145438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
 end
