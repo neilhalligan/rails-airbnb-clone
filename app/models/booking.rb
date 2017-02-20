55b2b395be6aa624185c4f9e5e87e113d7293164
John <giovannibitar@gmail.com>
2017-02-20 17:23:57 +0100

    added validations to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user_id
   belongs_to :car_id
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user_id, uniqueness: {scope: :car_id }
 end
