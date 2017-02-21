04a453986aea49e9a10b11937f8f555b2f5d2a97
John <giovannibitar@gmail.com>
2017-02-21 10:43:53 +0100

    views created fixing routes so booking can be created on car show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cars#index'
 
-  resources :cars
-  resources :bookings
+  resources :cars do
+    resources :bookings
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
