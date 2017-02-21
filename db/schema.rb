cb77d7348e023eb3ac0f99cc687d6a4df164dea0
Fatuma Khaireh <fkhaireh@gmail.com>
2017-02-21 18:31:28 +0100

    worked on show feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221125314) do
+ActiveRecord::Schema.define(version: 20170221144648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 20170221125314) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "user_image"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
