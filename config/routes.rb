aa2ca921d1d0e24c831edafe09646a353349c8c7
John <giovannibitar@gmail.com>
2017-02-21 11:48:54 +0100

    little things

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cars#index'
 
+  resources :bookings, only: [:show]
   resources :cars do
-    resources :bookings
+    resources :bookings, except: [:show]
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
