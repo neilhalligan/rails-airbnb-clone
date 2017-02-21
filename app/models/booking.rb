ad1fadcda1d4ed4b643b12bd8df5a4f97ce04705
John <giovannibitar@gmail.com>
2017-02-21 16:56:18 +0100

    model in place set up

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user_id
   belongs_to :car_id
 
+  has_many :reviews
+
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :user_id, uniqueness: {scope: :car_id }
