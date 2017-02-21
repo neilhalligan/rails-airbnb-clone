602727b6c2b855fc17b73d099ddd60f9172a0fdb
Neil Halligan <neilhalligan2972@gmail.com>
2017-02-21 18:37:54 +0100

    added dash stuff

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,13 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!, only: [ :dashboard ]
+
   def show
     @user = User.find(params[:id])
   end
 
   def dashboard
     # @bookings = Booking.all
-    # @bookings = current_user.bookings
+    @bookings = current_user.bookings
     # @bookings = current_user.(Booking.all)
     @cars = current_user.cars
     # @rental_cars = current_user.books.cars
