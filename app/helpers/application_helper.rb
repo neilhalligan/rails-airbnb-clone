8b4cc6d1367e1e96408f923eb7f06db191dfa298
Anders Ryden <rydena021@gmail.com>
2017-02-22 17:08:13 +0100

    prepare for deployment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user_signed_in? && current_user.facebook_picture_url
       current_user.facebook_picture_url
     else
-      "https://s.aolcdn.com/hss/storage/midas/627f1d890718ff2c58318a280145a153/203216448/nicholas-cage-con-air.jpg"
+      "https://wiki.cam.ac.uk/wiki/ajmorris/img_auth.php/4/41/Unknown_user.png"
     end
   end
 end
