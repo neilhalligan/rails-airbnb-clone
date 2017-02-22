33d89f6ccd82bf5fcbbefb3b0ceb39443d96d65e
Neil Halligan <neilhalligan2972@gmail.com>
2017-02-22 15:32:01 +0100

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-User.destroy_all
-Car.destroy_all
 Review.destroy_all
 Booking.destroy_all
+Car.destroy_all
+User.destroy_all
 
 # cars = [
 #     { brand: "Peugot", model: "235", description: "mint" },
