e6a0061f5e4a0a9bf6df01fea18013d21387808f
Anders Ryden <rydena021@gmail.com>
2017-02-22 14:56:33 +0100

    set cage as default user pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user_signed_in? && current_user.facebook_picture_url
       current_user.facebook_picture_url
     else
-      "http://placehold.it/30x30"
+      "https://s.aolcdn.com/hss/storage/midas/627f1d890718ff2c58318a280145a153/203216448/nicholas-cage-con-air.jpg"
     end
   end
 end
