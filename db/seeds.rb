d190db37174d6f32b4c79b26aea7762c3603d3fc
Neil Halligan <neilhalligan2972@gmail.com>
2017-02-22 15:29:18 +0100

    delete db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,22 @@
 
 User.destroy_all
 Car.destroy_all
+Review.destroy_all
+Booking.destroy_all
 
-cars = [
-    { brand: "Peugot", model: "235", description: "mint" },
-    { brand: "Ford", model: "F-150", description: "shitty" },
-    { brand: "Chevy", model: "Tahoe", description: "mint" },
-    { brand: "Audi", model: "A8", description: "great" },
-    { brand: "Jeep", model: "Cherokee", description: "worn" },
-    { brand: "Cadillac", model: "Escalade", description: "good" },
-]
+# cars = [
+#     { brand: "Peugot", model: "235", description: "mint" },
+#     { brand: "Ford", model: "F-150", description: "shitty" },
+#     { brand: "Chevy", model: "Tahoe", description: "mint" },
+#     { brand: "Audi", model: "A8", description: "great" },
+#     { brand: "Jeep", model: "Cherokee", description: "worn" },
+#     { brand: "Cadillac", model: "Escalade", description: "good" },
+# ]
 
-user = User.create!(name: "neil", email: "neil@gmail.com", password: "111111")
+# user = User.create!(name: "neil", email: "neil@gmail.com", password: "111111")
 
-cars.each do |car|
-  new_car = Car.new(car)
-  new_car.user = user
-  new_car.save!
-end
+# cars.each do |car|
+#   new_car = Car.new(car)
+#   new_car.user = user
+#   new_car.save!
+# end
