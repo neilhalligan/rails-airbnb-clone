90e8d8f84b86e61a14e37613d4ab394726eda446
Neil Halligan <neilhalligan2972@gmail.com>
2017-03-22 15:19:12 +0000

    fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :delete_all
 
   validates :car, presence: true
   validates :start_date, presence: true
